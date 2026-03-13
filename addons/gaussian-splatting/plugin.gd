a6e1c6229b5c7512c0dac0b91638f5623d5c9e0f
朱山童 <josonton@qq.com>
2026-03-13 14:06:14 +0800

    added node icons

--- addons/gaussian-splatting/plugin.gd
+++ addons/gaussian-splatting/plugin.gd
@@ -3,11 +3,13 @@ extends EditorPlugin
 
 const GaussianSplattingScript = preload("res://addons/gaussian-splatting/gaussian_splatting.gd")
 const SettingsOverrideScript = preload("res://addons/gaussian-splatting/gaussian_splatting_settings_override.gd")
+const GaussianSplattingIcon = preload("res://addons/gaussian-splatting/icons/gaussian_splatting.svg")
+const SettingsOverrideIcon = preload("res://addons/gaussian-splatting/icons/settings_override.svg")
 
 func _enter_tree() -> void:
 	add_autoload_singleton("GaussianSplattingServer", "res://addons/gaussian-splatting/gaussian_splatting_server.gd")
-	add_custom_type("GaussianSplatting", "Node3D", GaussianSplattingScript, null)
-	add_custom_type("GaussianSplattingServerSettingsOverride", "Node", SettingsOverrideScript, null)
+	add_custom_type("GaussianSplatting", "Node3D", GaussianSplattingScript, GaussianSplattingIcon)
+	add_custom_type("GaussianSplattingServerSettingsOverride", "Node", SettingsOverrideScript, SettingsOverrideIcon)
 
 func _exit_tree() -> void:
 	remove_autoload_singleton("GaussianSplattingServer")
